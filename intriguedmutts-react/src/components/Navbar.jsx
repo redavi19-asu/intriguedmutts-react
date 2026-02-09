8a9f33438e4da6d5e6376f5b6f59e77cdfb656ad
Ryan E Davis <redavi19@asu.edu>
2026-02-09 01:50:14 +0000

    e

--- intriguedmutts-react/src/components/Navbar.jsx
+++ intriguedmutts-react/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+export default function Navbar() {
   return (
     <nav className="w-full bg-gray-900 text-white p-6 text-center text-xl">
       INTRIGUED MUTTS
--- intriguedmutts-react/src/pages/Home.jsx
+++ intriguedmutts-react/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+export default function Home() {
   return (
     <div className="w-full">
       <section className="h-screen bg-black flex items-center justify-center text-5xl font-black">
